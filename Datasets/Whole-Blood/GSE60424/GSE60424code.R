498c790ea23cc6450026da92a5e050b174eb49a8
TanayJohary <tanya.johary@gmail.com>
2025-02-11 00:07:56 +0100

    update results for some datasets

--- Datasets/Whole-Blood/GSE60424/GSE60424code.R
+++ Datasets/Whole-Blood/GSE60424/GSE60424code.R
@@ -1,3 +1,8 @@
+################################################################################
+############################Prepare data for RF#################################
+#########################with already normalized TMM############################
+################################################################################
+
 # Retrieve the geo data from GEO
 geo_data <- getGEO("GSE60424", GSEMatrix = TRUE)
 
@@ -13,7 +18,6 @@ sub_pheno <- pheno_data[, c("title", "characteristics_ch1.4")]
 #read normalized count data
 expression_data <- read.delim("GSE60424_GEOSubmit_FC1to11_normalized_counts.txt", 
                               header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-
 
 # Connect to Ensembl biomart
 ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
@@ -92,9 +96,191 @@ write.csv(sepsis_data, "sepsis_dataGSE60424.csv", row.names = FALSE)
 
 
 ################################################################################
+############################Prepare data for RF#################################
+#########################with normalization by Deseq############################
+################################################################################
 
 # Read the raw dataset
 raw_data <- read.delim("GSE60424_raw_counts_GRCh38.p13_NCBI.tsv", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+rownames(raw_data) <- raw_data$GeneID
+raw_data <- raw_data[, -1]
+
+# Retrieve the geo data from GEO
+geo_data <- getGEO("GSE60424", GSEMatrix = TRUE)
+
+#Check the phenoData for information about the samples and prepare it for coldata in Deseq object
+pheno_data <- pData(geo_data[[1]])
+sub_pheno <- pheno_data[, c("geo_accession", "characteristics_ch1.4")]
+sub_pheno <- sub_pheno %>%
+ rename_with(~"Label", matches("characteristics_ch1.4"))
+sub_pheno <- sub_pheno %>%
+  mutate(Label = case_when(
+    Label == "diseasestatus: Sepsis" ~ "Sepsis",
+    Label == "diseasestatus: Healthy Control" ~ "healthy",
+    Label == "diseasestatus: Type 1 Diabetes" ~ "Diabetes",
+    Label == "diseasestatus: MS posttreatment" ~ "MS",
+    Label == "diseasestatus: MS pretreatment" ~ "MS",
+    Label == "diseasestatus: ALS" ~ "ALS",
+    TRUE ~ Label
+  ))
+sub_pheno$Label <- as.factor(sub_pheno$Label)
+sub_pheno$Label <- relevel(sub_pheno$Label, ref = "healthy")
+all(rownames(sub_pheno) %in% colnames(raw_data))
+all(colnames(raw_data) %in% rownames(sub_pheno))
+
+# Create DESeq2 object
+dds <- DESeqDataSetFromMatrix(
+  countData = raw_data,
+  colData = sub_pheno,
+  design =~ Label
+)
+
+# make normalization
+dds <- DESeq(dds)
+
+# Apply Variance Stabilizing Transformation (VST)
+vsd <- vst(dds, blind = FALSE)
+
+# Get normalized counts
+normalized_data <- assay(vsd)
+
+# Remove the gene with very low expression
+mean_expression <- rowMeans(normalized_data)
+filtered_norm <- normalized_data[mean_expression > 1, ] 
 
 # Read the annotation data for dataset
 annotation_data <- read.delim("Human.GRCh38.p13.annot.tsv", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+sub_annot <- annotation_data[, c(1,2)]
+
+
+# Merge the normalized data and sub annotation data to map the genes
+merged_norm <- merge(filtered_norm, sub_annot, by.x = "row.names", by.y = "GeneID", all.x = TRUE)
+write.csv(merged_norm, "normalized_DEseq_with_SymbolGSE60424.csv", row.names = FALSE)
+
+################################################################################
+############################## Genes of interest ###############################
+################################################################################
+
+# search for genes of interest
+genes_of_interest <- c(
+  "CALCA", "CRP", "TREM1", "PLAUR", "LBP", "IL6", "HMGB1", "ELANE", "CXCL8",
+  "IL10", "IL1B", "S100A8", "S100A9", "S100A12", "CD14", "FCGR1A", "ITGAM", "C3AR1",
+  "C5AR1", "LCN2", "CXCL10", "IFNG", "IFNA1", "IFNA2", "IFNB1", "CCL19", "CCL25", "CX3CR1",
+  "P2RX7", "PTX3", "TNFSF10", "MMP8", "MMP9", "HLA-DRA", "TNF", "MYD88", "NLRP3",
+  "TLR2", "TLR4", "NOTCH1", "BCL2", "PDCD1", "CCL2", "ARG1", "IL1R2", "CD177",
+  "OLFM4", "MAPK14", "VCAM1", "ICAM1", "SOCS3", "GATA3", "CCR7", "CCR2", "HIF1A"
+)
+
+present_genes <- genes_of_interest[genes_of_interest %in% merged_norm$Symbol]
+missing_genes <- genes_of_interest[!genes_of_interest %in% merged_norm$Symbol]
+
+# Filter our genes of interest
+filtered_genes <- merged_norm %>%
+  filter(Symbol %in% present_genes)
+filtered_genes <- filtered_genes[, -1]
+
+# Transpose the data
+transposed_data <- filtered_genes %>%
+  pivot_longer(
+    cols = -Symbol,
+    names_to = "Sample",
+    values_to = "Value"
+  ) %>%
+  pivot_wider(
+    names_from = Symbol,
+    values_from = Value
+  )
+
+# Merge to phenotype data to filter the sepsis patients
+merged_transposed <- merge(transposed_data, sub_pheno, by.x = "Sample", by.y = "geo_accession", all.x = TRUE)
+
+# Filter sepsis and helathy samples
+sepsis_data <- merged_transposed %>%
+  filter(Label %in% c("Sepsis", "healthy"))
+write.csv(sepsis_data, "sepsis_Deseq_GSE60424.csv", row.names = FALSE)
+
+################################################################################
+################################### 60 Genes  ##################################
+################################################################################
+
+genes_of_interest <- c(
+  "CALCA", "CRP", "TREM1", "PLAUR", "LBP", "IL6", "HMGB1", "ELANE", "CXCL8",
+  "IL10", "IL1B", "S100A8", "S100A9", "S100A12", "CD14", "FCGR1A", "ITGAM", "C3AR1",
+  "C5AR1", "LCN2", "CXCL10", "IFNG", "IFNA1", "IFNA2", "IFNB1", "CCL19", "CCL25", "CX3CR1",
+  "P2RX7", "PTX3", "TNFSF10", "MMP8", "MMP9", "HLA-DRA", "TNF", "MYD88", "NLRP3",
+  "TLR2", "TLR4", "NOTCH1", "BCL2", "PDCD1", "CCL2", "ARG1", "IL1R2", "CD177",
+  "OLFM4", "MAPK14", "VCAM1", "ICAM1", "SOCS3", "GATA3", "CCR7", "CCR2", "HIF1A",
+  "LY96", "CXCR4", "TRIL", "TLR5", "CXCL13"
+)
+
+present_genes <- genes_of_interest[genes_of_interest %in% merged_norm$Symbol]
+missing_genes <- genes_of_interest[!genes_of_interest %in% merged_norm$Symbol]
+
+# Filter our genes of interest
+filtered_genes <- merged_norm %>%
+  filter(Symbol %in% present_genes)
+filtered_genes <- filtered_genes[, -1]
+
+# Transpose the data
+transposed_data <- filtered_genes %>%
+  pivot_longer(
+    cols = -Symbol,
+    names_to = "Sample",
+    values_to = "Value"
+  ) %>%
+  pivot_wider(
+    names_from = Symbol,
+    values_from = Value
+  )
+
+# Merge to phenotype data to filter the sepsis patients
+merged_transposed <- merge(transposed_data, sub_pheno, by.x = "Sample", by.y = "geo_accession", all.x = TRUE)
+
+# Filter sepsis and helathy samples
+sepsis_data <- merged_transposed %>%
+  filter(Label %in% c("Sepsis", "healthy"))
+write.csv(sepsis_data, "sepsis_60Deseq_GSE60424.csv", row.names = FALSE)
+
+################################################################################
+################################# Selected genes ###############################
+################################################################################
+
+genes_of_interest <- c(
+  "CALCA", "TREM1", "PLAUR", "LBP", "IL6", "HMGB1", "ELANE", "CXCL8",
+  "IL10", "IL1B", "S100A8", "S100A9", "S100A12", "CD14", "FCGR1A", "ITGAM", "C3AR1",
+  "C5AR1", "LCN2", "CXCL10", "IFNG", "IFNA1", "IFNB1", "CCL25", "CX3CR1",
+  "P2RX7", "PTX3", "TNFSF10", "MMP8", "MMP9", "HLA-DRA", "TNF", "MYD88", "NLRP3",
+  "TLR2", "TLR4", "NOTCH1", "BCL2", "PDCD1", "CCL2", "ARG1", "IL1R2", "CD177",
+  "OLFM4", "MAPK14", "VCAM1", "ICAM1", "SOCS3", "GATA3", "CCR7", "CCR2", "HIF1A",
+  "LY96", "CXCR4", "TLR5"
+)
+
+
+present_genes <- genes_of_interest[genes_of_interest %in% merged_norm$Symbol]
+missing_genes <- genes_of_interest[!genes_of_interest %in% merged_norm$Symbol]
+
+# Filter our genes of interest
+filtered_genes <- merged_norm %>%
+  filter(Symbol %in% present_genes)
+filtered_genes <- filtered_genes[, -1]
+
+# Transpose the data
+transposed_data <- filtered_genes %>%
+  pivot_longer(
+    cols = -Symbol,
+    names_to = "Sample",
+    values_to = "Value"
+  ) %>%
+  pivot_wider(
+    names_from = Symbol,
+    values_from = Value
+  )
+
+# Merge to phenotype data to filter the sepsis patients
+merged_transposed <- merge(transposed_data, sub_pheno, by.x = "Sample", by.y = "geo_accession", all.x = TRUE)
+
+# Filter sepsis and helathy samples
+sepsis_data <- merged_transposed %>%
+  filter(Label %in% c("Sepsis", "healthy"))
+write.csv(sepsis_data, "sepsis_selected_Deseq_GSE60424.csv", row.names = FALSE)
+
